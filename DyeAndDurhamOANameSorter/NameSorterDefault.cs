2c0abed7c6cf842141e4af5e9fc8996b98fae023
David Wang <DavidXinWang@outlook.com>
2023-08-26 20:06:59 -0400

    Added and improved NameSorterDefault comments.

--- DyeAndDurhamOANameSorter/NameSorterDefault.cs
+++ DyeAndDurhamOANameSorter/NameSorterDefault.cs
@@ -1,14 +1,14 @@
 ﻿namespace DyeAndDurhamOANameSorter
 {
     /// <summary>
-    /// Sorts objects that implements IName. 
+    /// Sorts objects that implements IName. Results in a sorted list. 
     /// </summary>
     public class NameSorterDefault : INameSorter
     {
         private List<IName> names;
 
         /// <summary>
-        /// Default constructor. Creates an empty Names list.
+        /// Default constructor. Creates an empty names list.
         /// </summary>
         public NameSorterDefault()
         {
@@ -16,9 +16,9 @@
         }
 
         /// <summary>
-        /// ICollection constructor. Makes a shallow copy of the list, and stores it.
+        /// ICollection constructor. Makes a shallow copy of the ICollection, and stores as names.
         /// </summary>
-        /// <param name="names">List of objects that implement INames</param>
+        /// <param name="names">ICollection of objects that implement INames</param>
         public NameSorterDefault(ICollection<IName> names)
         {
             Replace(names);
@@ -31,7 +31,7 @@
         }
 
         /// <summary>
-        /// Add a name to the list.
+        /// Add a name to the names list.
         /// </summary>
         /// <param name="name">object that implements IName</param>
         public void Add(IName name)
@@ -39,20 +39,29 @@
             names.Add(name);
         }
 
+        /// <summary>
+        /// Removes a name from the names list. Note that the name object must be the same rather than the members.
+        /// </summary>
+        /// <param name="name">object that implements IName</param>
         public void Remove(IName name)
         {
             names.Remove(name);
         }
 
+        /// <summary>
+        /// Returns the list of sorted names cast as ICollection. Default sorting order of name used.
+        /// </summary>
+        /// <returns>sorted list of names cast as ICollection</returns>
         public ICollection<IName> GetResult()
         {
+            names.Sort();
             return names;
         }
 
         /// <summary>
-        /// Makes a shallow copy of the list, and store it.
+        /// Makes a shallow copy of the list, and store it as names.
         /// </summary>
-        /// <param name="names">List of objects that implement INames</param>
+        /// <param name="names">ICollection of objects that implement INames</param>
         public void Replace(ICollection<IName> names)
         {
             this.names = new List<IName>(names);
