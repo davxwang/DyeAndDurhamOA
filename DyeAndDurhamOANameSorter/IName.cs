f6c9f5db03c10cd07d66370a3426a84877d3663e
David Wang <DavidXinWang@outlook.com>
2023-08-26 20:08:50 -0400

    Added comments to IName.

--- DyeAndDurhamOANameSorter/IName.cs
+++ DyeAndDurhamOANameSorter/IName.cs
@@ -1,5 +1,9 @@
 ﻿namespace DyeAndDurhamOANameSorter
 {
+    /// <summary>
+    /// Interface for names. 
+    /// Inherits IComparable to enforce CompareTo implementation.
+    /// </summary>
     public interface IName : IComparable
     {
         string FullName { get; set; }
